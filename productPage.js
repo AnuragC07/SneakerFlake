762a40882c1dfc99e6ff53daab526c908a947642
AnuragC07 <anuragforwork0018@gmail.com>
2023-10-05 18:52:17 +0530

    commit

--- productPage.js
+++ productPage.js
@@ -1,5 +1,10 @@
 var mainImg = document.getElementById("main-img");
 var secondImg = document.getElementsByClassName("second-img");
+var quantityIndicator = document.getElementById("quantity");
+var plusBtn = document.getElementById("plusBtn");
+var minusBtn = document.getElementById("minusBtn");
+
+count = 0;
 
 secondImg[0].onclick = function(){
     mainImg.src = secondImg[0].src;
@@ -12,4 +17,21 @@ secondImg[1].onclick = function(){
 secondImg[2].onclick = function(){
     mainImg.src = secondImg[2].src;
     console.log("clicked");
+}
+
+plusBtn.onclick = function() {
+    if(count < 10){
+        count++;
+        console.log("hi");
+        quantityIndicator.innerHTML = "" + count;
+    }
+   
+}
+minusBtn.onclick = function() {
+    if(count > 0){
+        count--;
+        console.log("hi");
+        quantityIndicator.innerHTML = "" + count;
+    }
+    
 }
