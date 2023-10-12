4e0ae403802c438e1afd218173293d3af7b5a3f0
AnuragC07 <anuragforwork0018@gmail.com>
2023-10-12 23:00:44 +0530

    almost finished productpage

--- productPage.js
+++ productPage.js
@@ -6,8 +6,9 @@ var minusBtn = document.getElementById("minusBtn");
 const popUp = document.querySelector("#popupIcon");
 const box = document.querySelector("#box");
 const main = document.querySelector("#main");
+var menuitem = document.querySelector("#productSection");
 const navtop = document.querySelector("#navtop");
-
+var sizesBox = document.querySelectorAll("#size");
 
 var flag = 0;
 
@@ -16,22 +17,42 @@ popUp.addEventListener("mouseover", ()=> {
 
     if(flag == 0){
         box.setAttribute("style", "visibility: visible; transition: all 0.4s ease;");
-        console.log("h");
-        // flag = 1;
-        if(main.addEventListener("mouseover", ()=> {
-            box.setAttribute("style", "visibility: hidden");
-        }));
-        else if(navtop.addEventListener("mouseover", ()=> {
-            box.setAttribute("style", "visibility: hidden");
-        }));
+        console.log("on");
+        flag = 1;
+        // if(main.addEventListener("mouseover", ()=> {
+        //     box.setAttribute("style", "visibility: hidden");
+        // }));
+        // else if(menuitem.addEventListener("mouseover", ()=> {
+        //     box.setAttribute("style", "visibility: hidden");
+        // }));
+        // else if(navtop.addEventListener("mouseover", ()=> {
+        //     box.setAttribute("style", "visibility: hidden");
+        // }));
     }
-    // else{
-    //     box.setAttribute("style", "visibility: hidden");
-    //     console.log("h");
-    //     flag = 0;
-    // }
+    else if(flag == 1){
+        box.setAttribute("style", "visibility: hidden; transition: all 0.4s ease;");
+        console.log("off");
+        flag = 0;
+    }
+    
 })
 
+count = 0;
+sizesBox.forEach(function(size) {
+    
+        size.addEventListener('click', function() {
+            if(count == 0){
+            size.style.backgroundColor = 'black';
+            size.style.color = 'white';
+            count = 1;
+            }
+            else if(count == 1){
+                size.style.backgroundColor = 'white';
+                size.style.color = 'black';
+                count = 0;
+            }
+        });
+  });
 
 
 
---
+++ todo.md
@@ -0,0 +1,10 @@
+next have to do -- 
+in particular product page
+fix the top popup links, 
+
+create cart/bag page
+implement cart logic
+create wishlist page
+
+download more shoe pics
+copy paste all products files
